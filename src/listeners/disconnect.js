14034612652cf0ff5ce19bb3a9c36e7736c63604
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2023-05-21 22:59:10 +0300

    Update disconnect.js

--- src/listeners/disconnect.js
+++ src/listeners/disconnect.js
@@ -1,4 +1,5 @@
 const SocketListener = require("../SocketListener.js");
+const echoSocket = require("../echoSocket.js");
 const redis = require("../redis/index.js");
 const ftSearch = require("../utils/ftSearch.js");
 
